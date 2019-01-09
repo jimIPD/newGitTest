9155adbfcea3ba542ad6dc133c8086bcc97fd116
jimIPD <46352970+jimIPD@users.noreply.github.com>
2019-01-09 16:52:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Hey Rich Lets Raise the Bar!</h1>
+        <h1>Hey Rich Lets Raise the Bar! Really High</h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
